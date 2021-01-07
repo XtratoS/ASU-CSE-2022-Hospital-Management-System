758e7f68cf81047d9d530a120d8d47627a22ccb1
Mariam <mariamalaa015@gmail.com>
2021-01-08 00:09:09 +0200

    First commit

---
+++ frontend/src/patient/doctorf.js
@@ -0,0 +1,25 @@
+import React, {Component} from "react"
+//import {BrowserRouter , Route} from 'react-router-dom';
+class doctorf extends Component{
+  state={
+      name:''
+  }
+    
+        handleChange=(e)=>{
+            console.log(e.target.value)
+
+        }
+    render(){
+        return(
+           <div className="App">
+              Follow up Form:
+            <form>
+                <input type="text" onChange={this.handleChange}/>
+                <button>Submit</button>
+            </form>
+            {this.state.name}
+            </div>
+        );
+    }
+}
+export default doctorf;
