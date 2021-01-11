9318f71cb92a876c8bc65660ae0b750ae81445df
XtratoS <ultimate.nuker@yahoo.com>
2021-01-11 15:55:31 +0200

    created components and added the base files for the home view

---
+++ frontend/src/Header.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+class Header extends Component {
+    render() {
+        return (
+            <header>
+                <div class="container">
+                    <div id="head1">
+                        <h1>Welcome to our Hospital Managment System</h1>
+                    </div>
+                <nav className="main-nav">
+                    <ul>
+                        <li><a href="index.html">Home</a></li>
+                        <li><a href="index.html">About</a></li>
+                        <li><a href="index.html">Services</a></li>
+                    </ul>
+                </nav>
+                </div>
+            </header>
+        );
+    }
+}
+
+export default Header;
---
+++ frontend/src/Showcase.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Showcase extends Component {
+    render() {
+        return (
+            <div class="brand">
+                <h1>We are here to help!</h1>
+                <h2>Making Health Care Better Together</h2>
+                <h4>The most valuble thing is your health. We are working to expand our Services and help you achieve everything easier with just a click!</h4>
+            </div>
+        );
+    }
+}
+
+export default Showcase;
