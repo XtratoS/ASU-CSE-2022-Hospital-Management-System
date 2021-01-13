b52b99436967bfc19559134aa61423db1a1f7a9f
XtratoS <ultimate.nuker@yahoo.com>
2021-01-13 03:56:26 +0200

    Modified settings.py

--- hospital_management_system/hospital_management_system/settings.py
+++ hospital_management_system/hospital_management_system/settings.py
@@ -73,9 +73,9 @@ TEMPLATES = [
     },
 ]
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
